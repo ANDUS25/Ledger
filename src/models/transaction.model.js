bbdd28f3d4cbb3bd7e49dc02eb7832e870f21655
ANDUS25 <anandchavan113@gmail.com>
2026-02-24 07:33:45 +0530

    working on the transaction model for user payment

---
+++ src/models/transaction.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const transactionModel = new mongoose.Schema({
+  fromAccount: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Account",
+    required: [true, "Transaction must be associated with a from account"],
+    index: true,
+  },
+  toAccount: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Account",
+    required: [true, "Transaction must be associated with a to account"],
+    index: true,
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+      message: "Status must be either PENDING, COMPLETED, FAILED, or REVERSED",
+    },
+    default: "PENDING",
+  },
+  amount: {
+    type: Number,
+    required: [true, "Amount is required for the transaction"],
+    min: [0, "Amount must be a positive number"],
+  },
+  idempotencyKey: {
+    type: String,
+    unique: true,
+    index: true,
+    required: [true, "Idempotency key is required for the transaction"],
+  },
+});
